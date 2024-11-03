6afebdfa71bd13f5ec6aee09ad5c54136bff69bc
Darren <darren.daley@hotmail.co.uk>
2024-11-03 19:28:01 +0000

    wrote basic rectangle to rectangle collision
    I created a rectangle struct as well as creating rectangle to rectangle collision detection

---
+++ src/Collision/CollisionDetection.cs
@@ -0,0 +1,12 @@
+﻿using Flam.Shapes;
+
+namespace Flam.Collision;
+
+public class CollisionDetection
+{
+    public static bool RectangleCollidesRectangle(Rectangle rectangle1, Rectangle rectangle2) =>
+        rectangle1.Left < rectangle2.Right &&
+        rectangle1.Right > rectangle2.Left &&
+        rectangle1.Top < rectangle2.Bottom &&
+        rectangle1.Bottom > rectangle2.Top;
+}
